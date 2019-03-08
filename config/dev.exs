78eb62cbca4712aebdfa1f9153931f770958f29f
Sreenadh TC <kesav.tc8@gmail.com>
2019-03-08 16:36:04 +0530

    Config change

--- config/dev.exs
+++ config/dev.exs
@@ -7,8 +7,8 @@ use Mix.Config
 # watchers to your application. For example, we use it
 # with webpack to recompile .js and .css sources.
 config :hello_genserver, HelloGenserverWeb.Endpoint,
-  http: [port: {:system, "PORT"}],
-  # http: [port: 4000],
+  # http: [port: {:system, "PORT"}],
+  http: [port: 4000],
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
